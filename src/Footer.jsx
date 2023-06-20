47d48d8e03410ae903ecd80be7b75cc5eb4905be
LucassRodr1guess <84428281+LucassRodr1guess@users.noreply.github.com>
2023-06-20 20:09:33 -0300

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <>
+        <footer>
+        <h5>Vidas importam: Diga NÃO ao feminicídio e SIM à valorização da mulher.</h5>
+      </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+    return(
+        <div>
+            <header>
+        <h1>O Feminicídio</h1>
+      </header>
+        <section>
+          <h4>O que é o feminicídio?</h4>
+          <p>
+            O feminicídio é o assassinato de mulheres por razões de gênero. É uma forma extrema de violência de gênero que ocorre em diferentes contextos sociais e culturais.
+          </p>
+        </section>
+        <section>
+          <p>De acordo com a tabela que registra os casos de Feminicídio em São Paulo, vamos divulgar alguns dados para vocês terem noção de como o feminicídio deve ser combatido e que está em um nível alarmante e que não pode ser ignorado!!</p>
+        </section>
+        </div>
+    )
+}
+
+export default Header
